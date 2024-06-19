96287888f77827826aa4c65b34f0fa7a9d76810e
Ashu23052002 <ans23052002@gmail.com>
2024-06-19 23:32:41 +0530

    first

---
+++ Blog-App/src/appwrite/auth.js
@@ -0,0 +1,67 @@
+import conf from "../conf/conf.js";
+import { Client, Account, ID } from "appwrite";
+
+// creating class
+export class AuthService {
+  client = new Client();
+  account;
+
+  constructor() {
+    this.client
+      .setEndpoint(conf.appwriteUrl)
+      .setProject(conf.appwriteProjectId);
+    this.account = new Account(this.client);
+  }
+
+  async createAccount({ email, password, name }) {
+    try {
+      const userAccount = await this.account.create(
+        ID.unique(),
+        email,
+        password,
+        name
+      );
+
+      if (userAccount) {
+        return this.login(email, password);
+      } else {
+        console.log("Error in taken detail in create account");
+        return userAccount;
+      }
+    } catch (error) {
+      console.log("Error in create account");
+      throw error;
+    }
+  }
+
+  async login({ email, password }) {
+    try {
+      return await this.account.createEmailPasswordSession(email, password);
+    } catch (error) {
+      console.log("Error in login account");
+      throw error;
+    }
+  }
+
+  async getCurrentUser() {
+    try {
+      return await this.account.get();
+    } catch (error) {
+      console.log("Appwrite serive :: getCurrentUser :: error", error);
+    }
+
+    return null;
+  }
+  async logout() {
+    try {
+      await this.account.deleteSessions();
+    } catch (error) {
+      console.log("Appwrite serive :: logout :: error", error);
+    }
+  }
+}
+
+// create a object
+const authService = new AuthService();
+
+export default authService;
---
+++ Blog-App/src/components/Logo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Logo({width = '100px'}) {
+  return (
+    <div className='text-xl font-serif text-black-800'>Job-Blog</div>
+  )
+}
+
+export default Logo
